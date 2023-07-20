681fedbfaddee9609666714a1b41c34534ad16d9
willtech <wilman_vargas@misena.edu.co>
2023-07-20 17:10:32 -0500

    Función para mostrar destinos disponibles

--- buscador-destinos.js
+++ buscador-destinos.js
@@ -1,18 +1,44 @@
 /* Arreglo de Objetos */
 const destinosDeViaje = [
     {
-      nombre: 'Playa del Carmen',
-      costo: 1000,
+      nombre: 'Cartagena',
+      tipoTurismo: ["Playa", "Vida Nocturna","Cultura","Gastronomía"],
+      actividades: ["Fuerte San Felipe","Playas de Barú","Islas del Rosario","Centro Histórico"],
+      costo: 374,
       clima: 'Tropical',
     },
     {
-      nombre: 'París',
-      costo: 2000,
-      clima: 'Templado',
+      nombre: 'Bogotá',
+      tipoTurismo: ["Convenciones", "Negocios", "Compras", "Gastronomía", "Vida Nocturna"],
+      actividades: ['La Candelaria','Monserrate','Zona G','Usaquén'],
+      costo: 162,
+      clima: 'Frío',
     },
     {
-      nombre: 'Nueva York',
-      costo: 1500,
-      clima: 'Templado',
+      nombre: 'Medellín',
+      tipoTurismo: ["Compras", "Cultura", "Salud","Convenciones","Vida Nocturna"],
+      actividades: ['Guatapé','El Poblado','Jardín Botánico','Pueblito Paisa'],
+      costo: 220,
+      clima: 'Tropical',
     },
   ];
+
+  /* Mostrar destinos disponibles con una función, ciclo for y template string*/
+
+  console.log(``);
+  console.log(`----------------------------| Destinos Disponibles |----------------------------`);
+  function mostrarDestinos () {
+    for (let i = 0; i < destinosDeViaje.length; i++){
+      const destino = destinosDeViaje[i];
+      console.log(`...........................................................................`);
+      console.log(`Nombre: ${destino.nombre}`);
+      console.log(`Tipo de Turísmo: ${destino.tipoTurismo}`);
+      console.log(`Actividades: ${destino.actividades}`);
+      console.log(`Costo: $${destino.costo}`);
+      console.log(`Clima: ${destino.clima}`);
+    }
+  }
+
+  mostrarDestinos();
+
+  
