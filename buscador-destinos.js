35b37895bde03eb85b4a8311b5cbc649fca629bb
wilmanvargas <willvml@ip-192-168-1-102.ec2.internal>
2023-07-19 22:09:44 -0500

    Creando arreglo de objetos con información de los destinos de viaje

---
+++ buscador-destinos.js
@@ -0,0 +1,18 @@
+/* Arreglo de Objetos */
+const destinosDeViaje = [
+    {
+      nombre: 'Playa del Carmen',
+      costo: 1000,
+      clima: 'Tropical',
+    },
+    {
+      nombre: 'París',
+      costo: 2000,
+      clima: 'Templado',
+    },
+    {
+      nombre: 'Nueva York',
+      costo: 1500,
+      clima: 'Templado',
+    },
+  ];
